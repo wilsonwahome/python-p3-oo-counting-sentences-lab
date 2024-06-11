c1ea630da8a4a777b471637f5a48de8c71424aec
wahome <wahomewgitonga@gmail.com>
2024-06-11 18:29:39 +0300

    update

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,31 @@
-#!/usr/bin/env python3
-
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self._value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, val):
+        if not isinstance(val, str):
+            print("The value must be a string.")
+        else:
+            self._value = val
+
+    def is_sentence(self):
+        return self._value.endswith(".")
+
+    def is_question(self):
+        return self._value.endswith("?")
+
+    def is_exclamation(self):
+        return self._value.endswith("!")
+
+    def count_sentences(self):
+        import re
+        # Split based on '.', '!', and '?' followed by a space or end of string
+        sentences = re.split(r'[.!?](?:\s|$)', self._value)
+        # Filter out empty strings from the split result
+        sentences = list(filter(lambda x: x.strip() != '', sentences))
+        return len(sentences)
